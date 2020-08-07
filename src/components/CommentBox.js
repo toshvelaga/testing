615d5c07703465d35462fe2f8344316ddb80db43
Tosh Velaga <toshvelaga@gmail.com>
2020-08-07 00:58:28 -0700

    added commentbox tests

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
-    state = {  }
+    state = { comment: '' }
+
+    handleChange = (e) => {
+        this.setState({ comment: e.target.value })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+
+        this.setState({ comment: ' ' })
+    }
+
     render() { 
         return (  
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h4>Add a Comment</h4>
-                <textarea />
+                <textarea onChange={this.handleChange} value={this.state.comment}/>
                 <div>
                     <button>Submit Comment</button>
                 </div>
