0135ff94740191eab4c10a6ff393647a85978a7a
Tosh Velaga <toshvelaga@gmail.com>
2020-08-21 21:18:45 -0700

    added redux to component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
 
 class CommentBox extends Component {
-    state = { comment: '' }
+	state = { comment: "" };
 
-    handleChange = (e) => {
-        this.setState({ comment: e.target.value })
-    }
+	handleChange = (e) => {
+		this.setState({ comment: e.target.value });
+	};
 
-    handleSubmit = (e) => {
-        e.preventDefault()
+	handleSubmit = (e) => {
+		e.preventDefault();
 
-        this.setState({ comment: ' ' })
-    }
+		this.props.saveComment(this.state.comment);
 
-    render() { 
-        return (  
-            <form onSubmit={this.handleSubmit}>
-                <h4>Add a Comment</h4>
-                <textarea onChange={this.handleChange} value={this.state.comment}/>
-                <div>
-                    <button>Submit Comment</button>
-                </div>
-            </form>
-        );
-    }
+		this.setState({ comment: " " });
+	};
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<h4>Add a Comment</h4>
+				<textarea onChange={this.handleChange} value={this.state.comment} />
+				<div>
+					<button>Submit Comment</button>
+				</div>
+			</form>
+		);
+	}
 }
- 
-export default CommentBox;
+
+export default connect(null, actions)(CommentBox);
